03c917c33ae577a17beacdf2096eb99766c74792
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-08-27 23:37:35 +0300

    Create function to play single round of the game

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // random number 0, 1, 2
     const randomNumber = Math.floor(Math.random() * 3);;
@@ -23,3 +26,20 @@ function getHumanChoice() {
     return prompt("What's your choice rock, paper, scissors?");
 };
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    let roundResult;
+    if (humanChoice === computerChoice) {
+        humanScore++;
+        computerScore++;
+        roundResult = "Tie!";
+    } else if ((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "rock" && computerChoice === "scissors")) {
+        humanScore++;
+        roundResult = "Player Wins!";
+    } else {
+        computerScore++;
+        roundResult = "Computer Wins!";
+
+    }
+    return roundResult;
+}
